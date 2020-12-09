f30419002691bd8fa9e9c9faf6233b2ba9514df9
Bernd Hartzer <berndhartzer@gmail.com>
2020-12-09 17:25:09 +1000

    day 7 part 2

--- 07_handy_haversacks/07_handy_haversacks.go
+++ 07_handy_haversacks/07_handy_haversacks.go
@@ -2,6 +2,7 @@ package handy_haversacks
 
 import (
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -18,8 +19,8 @@ func HandyHaversacksPartOne(rules []string, target string) int {
 
 		containsSplit := strings.Split(split[1], ",")
 		for _, part := range containsSplit {
-			containsMatches := regexp.MustCompile("(\\d+) (.+) bag").FindStringSubmatch(part)
-			containee := containsMatches[2]
+			containsBags := regexp.MustCompile("(\\d+) (.+) bag").FindStringSubmatch(part)
+			containee := containsBags[2]
 
 			_, ok := all[containee]
 			if !ok {
@@ -44,4 +45,67 @@ func HandyHaversacksPartOne(rules []string, target string) int {
 	}
 
 	return len(unique)
+}
+
+type Bag struct {
+	name     string
+	contains map[string]int
+}
+
+func HandyHaversacksPartTwo(rules []string, target string) int {
+	all := make(map[string]*Bag)
+
+	for _, rule := range rules {
+		split := strings.Split(rule, " bags contain ")
+		if strings.Contains(split[1], "no other bags") {
+			continue
+		}
+
+		name := split[0]
+
+		bag := &Bag{
+			name:     name,
+			contains: make(map[string]int),
+		}
+
+		containsSplit := strings.Split(split[1], ",")
+		for _, part := range containsSplit {
+			containsBags := regexp.MustCompile("(\\d+) (.+) bag").FindStringSubmatch(part)
+			num, _ := strconv.Atoi(containsBags[1])
+			containee := containsBags[2]
+
+			bag.contains[containee] = num
+		}
+
+		all[name] = bag
+	}
+
+	bagsToCount := map[string]int{
+		target: 1,
+	}
+
+	count := 0
+	loop := true
+	for loop {
+		loop = false
+
+		for k, v := range bagsToCount {
+			if v > 0 {
+				loop = true
+
+				bag, ok := all[k]
+				if ok {
+					for kk, vv := range bag.contains {
+						bagsToCount[kk] += vv * v
+					}
+				}
+
+				count += v
+
+				bagsToCount[k] = 0
+			}
+		}
+	}
+
+	return count - 1
 }
--- 07_handy_haversacks/07_handy_haversacks_test.go
+++ 07_handy_haversacks/07_handy_haversacks_test.go
@@ -38,6 +38,56 @@ func TestHandyHaversacksPartOne(t *testing.T) {
 	})
 }
 
+func TestHandyHaversacksPartTwo(t *testing.T) {
+	t.Run("example input", func(t *testing.T) {
+		example := []string{
+			"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+			"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+			"bright white bags contain 1 shiny gold bag.",
+			"muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+			"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+			"dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+			"vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+			"faded blue bags contain no other bags.",
+			"dotted black bags contain no other bags.",
+		}
+		target := "shiny gold"
+
+		expected := 32
+		got := HandyHaversacksPartTwo(example, target)
+		if got != expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("example input two", func(t *testing.T) {
+		example := []string{
+			"shiny gold bags contain 2 dark red bags.",
+			"dark red bags contain 2 dark orange bags.",
+			"dark orange bags contain 2 dark yellow bags.",
+			"dark yellow bags contain 2 dark green bags.",
+			"dark green bags contain 2 dark blue bags.",
+			"dark blue bags contain 2 dark violet bags.",
+			"dark violet bags contain no other bags.",
+		}
+		target := "shiny gold"
+
+		expected := 126
+		got := HandyHaversacksPartTwo(example, target)
+		if got != expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("actual input", func(t *testing.T) {
+		input := readFileAsStringSlice(t, "./07_input.txt")
+		target := "shiny gold"
+
+		output := HandyHaversacksPartTwo(input, target)
+		t.Log(fmt.Sprintf("%d\n", output))
+	})
+}
+
 func readFileAsStringSlice(t *testing.T, name string) []string {
 	t.Helper()
 
