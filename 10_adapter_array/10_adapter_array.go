40bb9249de3658bc9ba04a85459381b763e940e4
Bernd Hartzer <berndhartzer@gmail.com>
2020-12-10 18:47:25 +1000

    day 10 part 2

--- 10_adapter_array/10_adapter_array.go
+++ 10_adapter_array/10_adapter_array.go
@@ -20,3 +20,16 @@ func AdapterArrayPartOne(adapters []int) int {
 
 	return differences[1] * differences[3]
 }
+
+func AdapterArrayPartTwo(adapters []int) int {
+	sort.Ints(adapters)
+	adapters = append(adapters, adapters[len(adapters)-1]+3)
+
+	variations := make(map[int]int)
+	variations[0] = 1
+	for _, n := range adapters {
+		variations[n] = variations[n-1] + variations[n-2] + variations[n-3]
+	}
+
+	return variations[adapters[len(adapters)-1]]
+}
--- 10_adapter_array/adapter_array_test.go
+++ 10_adapter_array/adapter_array_test.go
@@ -81,6 +81,79 @@ func TestAdapterArrayPartOne(t *testing.T) {
 	})
 }
 
+func TestAdapterArrayPartTwo(t *testing.T) {
+	t.Run("example input", func(t *testing.T) {
+		example := []int{
+			16,
+			10,
+			15,
+			5,
+			1,
+			11,
+			7,
+			19,
+			6,
+			12,
+			4,
+		}
+
+		expected := 8
+		got := AdapterArrayPartTwo(example)
+		if got != expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("example input two", func(t *testing.T) {
+		example := []int{
+			28,
+			33,
+			18,
+			42,
+			31,
+			14,
+			46,
+			20,
+			48,
+			47,
+			24,
+			23,
+			49,
+			45,
+			19,
+			38,
+			39,
+			11,
+			1,
+			32,
+			25,
+			35,
+			8,
+			17,
+			7,
+			9,
+			4,
+			2,
+			34,
+			10,
+			3,
+		}
+
+		expected := 19208
+		got := AdapterArrayPartTwo(example)
+		if got != expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("actual input", func(t *testing.T) {
+		input := readFileAsIntSlice(t, "./10_input.txt")
+
+		output := AdapterArrayPartTwo(input)
+		t.Log(fmt.Sprintf("%d\n", output))
+	})
+}
+
 func readFileAsIntSlice(t *testing.T, name string) []int {
 	t.Helper()
 
